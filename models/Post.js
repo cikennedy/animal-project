4660f348a7d13bbacffabf347bbae55734fc42c9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-08 11:37:51 -0500

    added Post model

---
+++ models/Post.js
@@ -0,0 +1,45 @@
+// requirements for model 
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create post model 
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        postTitle: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        },
+        postContent: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        postPhoto : {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+       
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+module.exports = Post;
