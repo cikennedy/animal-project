9cab368d270a290c4321db1acaaacb872497a40a
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 19:49:20 -0500

    updated models and routes with proper requirements and information

--- models/index.js
+++ models/index.js
@@ -1,11 +1,17 @@
 const User = require('./User');
 const Post = require('./Post');
+const Reply = require('./Reply')
 
 User.hasMany(Post, {
     foreignKey: 'user_id',
 });
 
+Post.hasMany(Reply, {
+    foreignKey: 'post_id',
+});
+
 module.exports = {
     User,
     Post,
+    Reply,
 };
