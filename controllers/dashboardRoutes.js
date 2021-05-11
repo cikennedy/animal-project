68bf9143eeba89a665d3956ec2fbd0e26f23d8cd
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 19:58:19 -0500

    entered dummy dashboardRoutes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+const { User, Post, Reply } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Find lost and found posts by the user that is logged in
+router.get('/', withAuth, (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Reply,
+                attributes: [
+                    'id',
+                    'reply',
+                    'post_id',
+                    'created_at'
+                ],
+            },
+        ]
+    })
+    .then(postData => {
+        const posts = postData.map(post => post.get({ plain: true }));
+        // Use dashboard template 
+        res.render('dashboard', {
+            posts,
+            email: req.session.email,
+            username: req.session.username,
+            user_id: req.session.user_id,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
-const userRoutes = require('./api/userRoutes');
-const homeRoutes = require('./api/homeRoutes');
-// const userRoutes = require('');
 
-router.use('/api/users', userRoutes);
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/api', apiRoutes);
 
-// router.use('', userRoutes);
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
