77c74140c6934ad012440ef52a8699664f2bf941
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-12 17:59:03 -0500

    updated dashboard routes and auth util

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Reply } = require('../models');
+const { Post, Reply } = require('../models');
 const withAuth = require('../utils/auth');
 
 // Find lost and found posts by the user that is logged in
@@ -12,6 +12,7 @@ router.get('/', withAuth, (req, res) => {
             'id',
             'post_title',
             'post_content',
+            'post_photo',
             'created_at'
         ],
         include: [
@@ -31,9 +32,6 @@ router.get('/', withAuth, (req, res) => {
         // Use dashboard template 
         res.render('dashboard', {
             posts,
-            email: req.session.email,
-            username: req.session.username,
-            user_id: req.session.user_id,
             loggedIn: true
         });
     })
@@ -42,5 +40,52 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
     });
 });
+
+// Edit lost and found post by selecting the id of the blog post 
+router.get('/edit/:id', withAuth, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_title',
+            'post_content',
+            'post_photo',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Reply,
+                attributes: [
+                    'id',
+                    'reply',
+                    'post_id',
+                    'created_at'
+                ],
+            },
+        ]
+    })
+    // Error if no lost and found post exists with the id 
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with the given id.'});
+            return;
+        }
+
+        // Use edit-post handlebars template 
+        const post = dbPostData.get({ plain: true });
+        res.render('id-edit-post', {
+            post,
+            loggedIn: true
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 
 module.exports = router;
