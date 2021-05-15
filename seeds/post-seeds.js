881e3c9d46c1b3c0cdb637ec1c5f2eda7f00382b
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-15 12:04:38 -0500

    changed seeds files to fit models, changed handlebars posts files to properly include photo url

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,10 +2,20 @@ const { Post } = require('../models')
 
 const postData = [
   {
-    post_title: "Test",
+    post_title: "Toby",
+    post_location: "Police station",
     post_content: "This is a test",
-    post_photo: "test.jpg",
+    animal_type: "Dog",
+    post_photo: "https://cdn.choosechicago.com/uploads/2020/09/Viceroy-Chicago-Pet-Friendly-Dog-Bed-1536x864.jpg",
     user_id: "1"
+  },
+  {
+    post_title: "Max",
+    post_location: "Park",
+    post_content: "This is another test",
+    animal_type: "Dog",
+    post_photo: "https://www.humanesociety.org/sites/default/files/styles/2000x850/public/2018/08/puppy-410265.jpg?h=0c7c9985&itok=KxhG3DVU",
+    user_id: "2"
   },
 ];
 
