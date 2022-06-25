f9728a755c602bdccaaa38aae6ac617430636597
Chris Kennedy <kennedy.christopherian@gmail.com>
2022-06-25 13:02:05 -0500

    components directory added, template rafce components added

---
+++ find-my-pet-ts/components/Layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+type Props = {
+    children?: ReactNode
+    title?: string
+  }
+
+const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
